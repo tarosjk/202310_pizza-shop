0c46d8ae4daf7c7f11e02eafbefe8b4e5dca9c8f
tarosjk <tarosjk@gmail.com>
2024-02-16 10:18:38 +0900

    エラーメッセージ関数を追加

--- add.php
+++ add.php
@@ -22,6 +22,26 @@ $errors = [
   ],
 ];
 
+$error_msg = [
+  'required' => '必須入力です',
+  'format' => '日本語、英数字のみ有効です。記号等は使用できません'
+];
+
+function error_msg($key)
+{
+  global $errors;
+  global $error_msg;
+  $error_html = '';
+
+  foreach ($errors[$key] as $key => $flag) {
+    if ($flag) {
+      $error_html .= "<p class=\"text-danger error-msg\">{$error_msg[$key]}</p>";
+    }
+  }
+
+  return $error_html;
+}
+
 // 1. 送信のチェック
 if (
   isset($_POST['submit']) &&
@@ -30,21 +50,37 @@ if (
 
   // 2. エラーチェック
   if (empty($_POST['pizza-name'])) {
+    $errors['pizza-name']['required'] = true;
   } else {
     // 入力があった場合
+
+    // 書式チェック
+    if (!preg_match('/^([^\x01-\x7E]|[\da-zA-Z ])+$/', $_POST['pizza-name'])) {
+      $errors['pizza-name']['format'] = true;
+    }
   }
 
   if (empty($_POST['chef-name'])) {
+    $errors['chef-name']['required'] = true;
   } else {
     // 入力があった場合
+
+    // 書式チェック
+    if (!preg_match('/^([^\x01-\x7E]|[\da-zA-Z ])+$/', $_POST['chef-name'])) {
+      $errors['chef-name']['format'] = true;
+    }
   }
 
   if (empty($_POST['topping'])) {
-  } else {
-    // 入力があった場合
-
+    $errors['topping']['required'] = true;
   }
-}
+
+  // エラーの内容確認
+  echo '<pre>';
+  var_dump($errors);
+  echo '</pre>';
+} // 送信チェック if
+
 
 ?>
 <?php
@@ -61,16 +97,18 @@ include 'template/header.php';
           <label for="pizza-name" class="form-label fw-bold">ピザの名前</label>
           <input type="text" name="pizza-name" id="pizza-name" placeholder="マルゲリータ" class="form-control">
           <p class="form-text">100文字以内で入力</p>
-          <p class="text-danger"><?= $error; ?></p>
+          <?= error_msg('pizza-name'); ?>
         </div>
         <div class="mb-3">
           <label for="chef-name" class="form-label fw-bold">シェフの名前</label>
           <input type="text" name="chef-name" id="chef-name" placeholder="ピザシェフ" class="form-control">
           <p class="form-text">ピザの作者の名前を入力して下さい。ニックネームもOK。</p>
+          <?= error_msg('chef-name'); ?>
         </div>
         <div class="mb-3">
           <label for="topping" class="form-label fw-bold">トッピング</label>
           <input type="text" name="topping" id="topping" placeholder="チーズ,バジル" class="form-control">
+          <?= error_msg('topping'); ?>
         </div>
         <div class="text-center">
           <button class="btn btn-primary" value="submit" name="submit">追加する</button>
