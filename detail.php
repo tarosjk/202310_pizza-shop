4a907e0a964919ea062ef8080ad62dd3b96f8de1
tarosjk <tarosjk@gmail.com>
2024-02-17 12:01:59 +0900

    削除の仕組みまで完了

--- detail.php
+++ detail.php
@@ -4,21 +4,65 @@ session_start();
 // データベース接続
 require 'config/dbconnect.php';
 
+// 削除
+if (
+  isset($_POST['delete']) &&
+  $_POST['delete'] === 'delete'
+) {
+  $stmt = $db->prepare('DELETE FROM pizzas WHERE id = ?');
+  $stmt->bindValue(1, $_POST['delete-id']);
+
+  if ($stmt->execute() && $stmt->rowCount()) {
+    $_SESSION['success-msg'] = 'ピザを削除しました';
+    header('location:index.php');
+    exit;
+  }
+}
+
+
+// URLパラメーターがない場合（TOPページにリダイレクト）
+if (!isset($_GET['id'])) {
+  header('location:index.php');
+  exit;
+}
+
 $stmt = $db->prepare('SELECT * FROM pizzas WHERE id = ?');
 $stmt->bindValue(1, $_GET['id']);
 $result = $stmt->execute();
 
 if ($result) {
   $pizza = $stmt->fetch();
-  var_dump($pizza);
 }
 
 include 'template/header.php';
 ?>
 
 <div class="container">
-  <h2 class="text-center display-4 my-5">Our Special Pizzas</h2>
+  <h2 class="text-center display-4 my-5">Pizza Detail</h2>
 
+  <h3 class="text-center mb-5"><?= htmlspecialchars($pizza['pizzaname']) ?></h3>
+
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <p class="fw-bold">トッピング:</p>
+          <p><?= htmlspecialchars($pizza['topping']); ?></p>
+          <p>シェフ: <?= htmlspecialchars($pizza['chefname']); ?></p>
+          <?php
+          $created_at = date('Y年n月j日 G時i分s秒', strtotime($pizza['created_at']));
+          ?>
+          <p class="text-secondary">登録日: <?= htmlspecialchars($created_at); ?></p>
+        </div>
+        <div class="card-footer text-end">
+          <form action="detail.php" method="post">
+            <input type="hidden" name="delete-id" value="<?= htmlspecialchars($pizza['id']); ?>">
+            <button name="delete" value="delete" class="btn btn-danger">この商品を削除する</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </div>
 
